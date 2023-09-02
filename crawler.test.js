ead2460f893f22791f167bf9485f6ff7b07ad8d4
nonspada <alistesalas@gmail.com>
2023-09-02 18:24:34 -0400

    added crawler functions and tests

--- crawler.test.js
+++ crawler.test.js
@@ -1,5 +1,6 @@
-const { normalizeURL } = require("./crawler");
+const { normalizeURL, getURLsFromHTML } = require("./crawler");
 const { test, expect } = require("@jest/globals");
+
 
 test("normalizeURL strip protocol", () => {
   const input = "https://en.wikipedia.org/wiki/Big_O_notation";
@@ -26,5 +27,70 @@ test("normalizeURL strip http", () => {
   const input = "http://en.wikipedia.org/wiki/Big_O_notation";
   const actual = normalizeURL(input);
   const expected = "en.wikipedia.org/wiki/Big_O_notation";
+  expect(actual).toEqual(expected);
+});
+
+// Get URL from HTML
+test("getURLsFromHTML absolute", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="https://en.wikipedia.org/wiki/Big_O_notation">Wikipedia Big O Notation</a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = "https://en.wikipedia.org/wiki/Big_O_notation";
+
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://en.wikipedia.org/wiki/Big_O_notation"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="/wiki/Big_O_notation">Wikipedia Big O Notation</a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = "https://en.wikipedia.org";
+
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://en.wikipedia.org/wiki/Big_O_notation"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML both", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="https://en.wikipedia.org/wiki/Big_O_notation">Wikipedia Big O Notation</a>
+      <a href="/wiki/Computer_science">Wikipedia Computer Science</a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = "https://en.wikipedia.org";
+
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [
+    "https://en.wikipedia.org/wiki/Big_O_notation",
+    "https://en.wikipedia.org/wiki/Computer_science",
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid url", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="Big_O_notation">Invalid URL</a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = "https://en.wikipedia.org/wiki/Big_O_notation";
+
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
   expect(actual).toEqual(expected);
 });
