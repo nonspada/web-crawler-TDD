89f4acbcda349df945d6e1f361aafc40d2aad05e
nonspada <alistesalas@gmail.com>
2023-09-03 13:02:20 -0300

    added recursive crawl function

--- crawler.js
+++ crawler.js
@@ -1,6 +1,20 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+
+  if (currentURLObj.hostname !== baseURLObj.hostname) {
+    return pages;
+  }
+
+  const normalizedCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++;
+    return pages;
+  }
+
+  pages[normalizedCurrentURL] = 1;
   console.log(`Actively crawling: ${currentURL}`);
 
   try {
@@ -10,7 +24,7 @@ async function crawlPage(currentURL) {
       console.log(
         `Error in fetch with status code ${resp.status} on page ${currentURL}`
       );
-      return;
+      return pages;
     }
 
     const contentType = resp.headers.get("content-type");
@@ -18,13 +32,20 @@ async function crawlPage(currentURL) {
       console.log(
         `No HTML response. Content type: ${contentType}, on page ${currentURL}`
       );
-      return;
+      return pages;
     }
 
-    console.log(await resp.text());
+    const htmlBody = await resp.text();
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (error) {
     console.log(`Error on fetching ${currentURL}: ${error.message}`);
   }
+  return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
